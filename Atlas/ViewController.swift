857c49bf4e6f6008da9698a2294319f4bb67848f
Pavel Belevtsev <pbelevtsev@hotmail.com>
2018-12-26 17:25:52 +0200

    Initial commit

---
+++ Atlas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Atlas
+//
+//  Created by Pavel Belevtsev on 12/26/18.
+//  Copyright © 2018 Pavel Belevtsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
